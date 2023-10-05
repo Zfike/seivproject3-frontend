30a459857800dc8425bc2ef3ada3893a58896c64
JaxenM <jxnmcray@gmail.com>
2023-10-05 12:31:48 -0500

    Changed from project2 to project3

--- vue.config.js
+++ vue.config.js
@@ -2,6 +2,6 @@ module.exports = {
     devServer: {
       host: 'localhost',
     },
-    publicPath: process.env.NODE_ENV === 'development' ? '/' : '/seivproject2-frontend/',
+    publicPath: process.env.NODE_ENV === 'development' ? '/' : '/seivproject3-frontend/',
     transpileDependencies: ['vuetify'],
   };
